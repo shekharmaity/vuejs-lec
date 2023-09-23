b54549e7137d0f5a9c94cdbbd21e44a9501f6746
Shekhar Maity <sheku@1921680104.local>
2023-09-23 15:11:34 +0530

    v-bind demo

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+  <div>
+    <p>Hello {{ firstName }}.</p>
+    <h2 v-text="firstName"></h2>
+    <h2 v-html="htmlSyntaxCode"></h2>
 
+    <!-- v-bind -->
+
+    <p v-bind:id="paraId">Vue Js Practice</p>
+
+    <p v-bind:disabled="isDisabled">Vue Js Practice</p>
+
+    <!-- conditional -->
+    <p :class="true && classCss">Class test</p>
+
+    <p :class="[classCss, two]">Array css</p>
+
+    <p v-bind:class="{
+      classCss
+    }">Object css</p>
+
+
+<p :style="sty">external css</p>
+
+  </div>
+</template>
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+
+  data() {
+    return {
+      firstName: "Shekhar",
+      header: "heading",
+      htmlSyntaxCode: "<b>Bold data</b>",
+      paraId: "para",
+      isDisabled: true,
+      classCss: "test",
+      two: "two",
+      sty:{
+        color: "blue",
+        padding: "20PX",
+      }
+    }
+  },
+  // components: {
+  //   HelloWorld
+  // }
 }
 </script>
 
@@ -22,5 +59,17 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#para {
+  color: blue;
+}
+
+.test {
+  background-color: blanchedalmond;
+}
+
+.two {
+  color: blue;
 }
 </style>
