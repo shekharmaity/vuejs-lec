ae6dbab5de5c3d2f7b81bd9b58a5cda3068ac77d
Shekhar Maity <sheku@1921680104.local>
2023-09-23 15:57:56 +0530

    for loop demo

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h2>Hello</h2>
+    Number : <input type="number" name="" id="" v-model="num">
+    <p v-if="num === 0">Value is zero</p>
+    <p v-else-if="num > 0"> Number is positive</p>
+    <p v-else-if="num < 0"> Number is negative</p>
+    <p v-else> Not a number</p>
+    <p v-if="isShow">Element is shows by v-if</p>
+    <p v-show="isShow">Element is shows by v-show</p>
+
+
+    <p v-for="(name, index) in names" :key="name">{{ index }} - {{ name }}</p>
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      num: 2,
+      isShow: true,
+      names: ['ravi', 'ram', 'sam', 'tejas'],
+      fullName: [
+        { first: "ravi", lastname: "singh" },
+        { first: "rahul", lastname: "sharma" },
+        { first: "ravi", lastname: "singh" },
+        { first: "ravi", lastname: "singh" }
+      ],
+
+      //  ravi singh
+      //  rahul sharma
+      order: {
+        id: 1,
+        price: 12000,
+        items: [
+          { id: 1, itemName: "mobile" },
+          { id: 2, itemName: "trimmer" },
+          { id: 3, itemName: "pillow" },
+        ]
+      }
+      // items names
+      // mobile
+      // trimmer
+      // pillow
+    }
+  },
+
 }
 </script>
 
