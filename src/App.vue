06191ca4e9f06506e85fbe7f988a31414c888b1b
Shekhar Maity <sheku@1921680104.local>
2023-09-27 21:06:14 +0530

    form example

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,35 @@
 <template>
-  
-  
+  <div>
+    <pre>
+    {{ JSON.stringify(formData, null, 2) }}
+    <h3>Form Example</h3>
+  </pre>
+    <form>
+      <div>
+        <label for="name">Name</label>
+        <input type="text" name="name" id="name" v-model="formData.name">
+      </div>
+      <br>
+      <div>
+        <label for="description">Description</label>
+        <textarea name="description" id="description" cols="30" rows="10" v-model="formData.description"></textarea>
+      </div>
+      <div>
+        <label for="country">Country : </label>
+        <select name="country" id="country" v-model="formData.country">
+          <option :value="country" v-for="(country, index) in countrys" :key="index">{{ country }}</option>
+        </select>
+      </div>
+      <div>
+        <label for="countrys">
+          <select name="countrys" id="countrys" multiple v-model="formData.countrys">
+          <option :value="country" v-for="(country, index) in countrys" :key="index">{{ country }}</option>
+        </select>
+        </label>
+      </div>
+
+    </form>
+  </div>
 </template>
 
 <script>
@@ -8,7 +37,17 @@
 
 export default {
   name: 'App',
-  
+  data() {
+    return {
+      countrys: ['india', 'usa', 'russia', 'japan'],
+      formData: {
+        name: "",
+        description: "",
+        country:"",
+        countrys:[],
+      }
+    }
+  },
 }
 </script>
 
