d58d5d0dae919c8ece9e4be1751abb953164e5e1
Shekhar Maity <sheku@1921680104.local>
2023-09-28 16:02:24 +0530

    computed lab

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,33 @@
 <template>
-  
-  
+  <div>
+    <h2>Vue lab</h2>
+
+    <label for="fullName">FullName : </label>
+    <p>{{ firstName }} - {{ lastName }}</p>
+    <p>{{ firstName }} - {{ lastName }}</p>
+    <p>{{ firstName }} - {{ lastName }}</p>
+    <p>{{ firstName }} - {{ lastName }}</p>
+    <p>{{ firstName }} - {{ lastName }}</p>
+
+
+
+    Method {{ fullName() }}
+
+
+
+    <br>
+
+    Computed {{ name }}
+
+    <br>
+    <input type="text" name="subject" id="subject" v-model="subject">
+
+    {{ subject }}
+
+    <br>
+    <br>
+    <input type="text" name="subject" id="subject" v-model="firstName">
+  </div>
 </template>
 
 <script>
@@ -8,17 +35,31 @@
 
 export default {
   name: 'App',
-  
+
+
+  data() {
+    return {
+      firstName: "Shekhar",
+      lastName: "Maity",
+      subject: "",
+    }
+  },
+
+  methods: {
+
+    fullName() {
+      console.log("fullName method called");
+      return this.firstName + "-" + this.lastName;
+    }
+  },
+
+  computed: {
+    name() {
+      //Busness logic
+      console.log("name computed called");
+      return this.firstName + "-" + this.lastName;
+    }
+  }
+
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
