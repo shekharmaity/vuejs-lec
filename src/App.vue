c316fbb05cdf6eaa42b0cf132d459bb3198ab6cf
Shekhar Maity <sheku@1921680104.local>
2023-10-01 12:39:17 +0530

    component labs demo

--- src/App.vue
+++ src/App.vue
@@ -9,39 +9,40 @@
     <p>{{ firstName }} - {{ lastName }}</p>
     <p>{{ firstName }} - {{ lastName }}</p>
 
-
-
     Method {{ fullName() }}
-
-
-
     <br>
 
     Computed {{ name }}
 
     <br>
     <input type="text" name="subject" id="subject" v-model="subject">
-
     {{ subject }}
-
     <br>
     <br>
     <input type="text" name="subject" id="subject" v-model="firstName">
+
+    <br><br>
+    <login-form :firstName="firstName" :lastName="formData.lastName"></login-form>
+
   </div>
 </template>
 
 <script>
-
+import LoginForm from './components/LoginForm.vue';
 
 export default {
+  components: { LoginForm },
   name: 'App',
-
 
   data() {
     return {
       firstName: "Shekhar",
       lastName: "Maity",
       subject: "",
+      formData: {
+        firstName: "Shekhar",
+        lastName: "Maity"
+      }
     }
   },
 
---
+++ src/components/LoginForm.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <form action="">
+            <div>
+                <label for="">First Name : </label>
+                <input type="text" name="name" id="" v-model="formData.firstName">
+            </div>
+            <div>
+                <label for="lastName">Last Name : </label>
+                <input type="text" name="lastName" id="" v-model="formData.lastName">
+            </div>
+            <button>Save</button>
+        </form>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "loginForm",
+    data() {
+        return {
+            formData: {
+                firstName: "",
+                lastName: "",
+            },
+        }
+    },
+    props: ['firstName', 'lastName'],
+
+    created() {
+        console.log("on mount");
+        this.formData.firstName = this.firstName;
+        this.formData.lastName = this.lastName;
+    }
+
+
+}
+</script>
