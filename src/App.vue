f3d669e6e941f6ec1916ef0534ed44edaa626a9b
Shekhar Maity <sheku@1921680104.local>
2023-10-02 15:11:03 +0530

    feature component

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <template>
-  <div></div>
-  
-</template>
+  <div style="text-align: center;">
+    <h2>App Page</h2>
+    <input type="text" name="firtName" id="firstName" v-model="firstName">
 
+    <top-nav :firstName="firstName" :lastName="true"></top-nav>
+   
+  </div>
+</template>
 <script>
 
-
+import TopNav from './components/TopNav.vue'  // step 1 import
 export default {
+  components: { TopNav },   //step 2 declare component
   name: 'App',
-
   data(){
     return{
-      
+      firstName:"",
     }
   }
-  
 }
 </script>
 
---
+++ src/components/TopNav.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>Top Menu</h1>
+        FirstName : {{ firstName }} {{ lastName }}
+    </div>
+</template>
+<script>
+export default {
+    name: "TopNav",
+    // props:['firstName', 'lastName'],
+    props: {
+        firstName: {
+            type: Boolean,
+            required: true,
+            default: false
+        },
+        lastName: {
+            type: Boolean,
+        }
+    },
+    data() {
+        return {
+        }
+    }
+}
+</script>
+
