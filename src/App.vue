b18f9de690577fccf76325aa445498376cf35681
Shekhar Maity <sheku@1921680104.local>
2023-10-14 12:46:59 +0530

    feature emit lecture

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,34 @@
 <template>
-  <div></div>
-  
+  <div>
+    <button @click="popUpFlag = true">Show Popup</button>
+    <pop-up v-show="popUpFlag" @close="closePopUp"></pop-up>
+    <p>UserName is : {{ userName }}</p>
+
+  </div>
 </template>
 
 <script>
+import PopUp from './components/PopUp.vue'
 
 
 export default {
+  components: { PopUp },
   name: 'App',
 
-  data(){
-    return{
-      
+  data() {
+    return {
+      popUpFlag: false,
+      userName:"",
+    }
+  },
+  methods: {
+    closePopUp(userName) {
+      console.log(userName);
+      this.userName= userName;
+      this.popUpFlag = false;
     }
   }
-  
+
 }
 </script>
 
---
+++ src/components/PopUp.vue
@@ -0,0 +1,23 @@
+<template>
+    <div style="text-align: center;">
+        <h2>This is a popup component</h2>
+        <label for="name">Name : </label>
+        <input type="text" name="name" id="name" v-model="userName">
+        <button @click="$emit('close', userName)">Close PopUp</button>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PopUp",
+    emits:['close'],
+    data(){
+        return{
+            userName:"",
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
