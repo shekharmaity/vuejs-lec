cd8f6345d9a52c118e6ff22df5ab2d5a7327421d
Shekhar Maity <sheku@1921680104.local>
2023-10-21 13:57:13 +0530

    feature-slots

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,40 @@
 <template>
-  <div></div>
-  
+  <div style="text-align: center;">
+    <p>Slot Example App page</p>
+    <!-- <slot-comp>
+      <h2>Hello</h2>
+    </slot-comp> -->
+
+    <slot-comp>
+      <template v-slot:header>
+        <h2>Header</h2>
+      </template>
+      <template v-slot:default>
+        <h2>Default template</h2>
+      </template>
+      <template v-slot:footer>
+        <h2>Footer</h2>
+      </template>
+
+    </slot-comp>
+
+
+
+  </div>
 </template>
 
 <script>
-
+import SlotComp from './components/SlotComp.vue'
 
 export default {
+  components: { SlotComp },
   name: 'App',
 
-  data(){
-    return{
-      
+  data() {
+    return {
+
     }
   }
-  
 }
 </script>
 
---
+++ src/components/SlotComp.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div id="card-header">
+            <slot name="header"></slot>
+        </div>
+        <div>
+            <slot></slot>
+        </div>
+        <div id="card-footer">
+            <slot name="footer"></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "SlotComp"
+}
+</script>
+<style lang="scss" scoped></style>
