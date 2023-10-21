c1425ec625d534549c4f985cb711ee3df6fd83d3
Shekhar Maity <sheku@1921680104.local>
2023-10-21 15:38:06 +0530

    feature user form

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,46 @@
 <template>
-  <div></div>
-  
+  <div style="text-align: center;">
+    <h2> User form</h2>
+
+    <button @click="isAdd = true">Add user</button>
+    <button @click="isEdit = true">Edit User</button>
+
+    <user-form title="Add" @addUser="addUser" v-show="isAdd"></user-form>
+    <user-form title="Edit" @editUser="editUser" :existingPerson="existingPerson" v-show="isEdit"></user-form>
+  </div>
 </template>
 
 <script>
-
-
+import UserForm from './components/UserForm.vue'
 export default {
+  components: { UserForm },
   name: 'App',
 
-  data(){
-    return{
-      
+  data() {
+    return {
+      isAdd: false,
+      isEdit: false,
+
+      existingPerson: {
+        fName: "vishal",
+        lName: "sharma"
+      }
     }
-  }
-  
+  },
+  methods: {
+    addUser(userData) {
+      console.log("addUser" + userData.fName);
+      this.isAdd = false;
+      //http backend
+    },
+
+    editUser(userData) {
+      console.log("addUser" + userData.fName);
+      this.isEdit = false;
+    }
+
+  },
+
 }
 </script>
 
---
+++ src/components/UserForm.vue
@@ -0,0 +1,76 @@
+<template>
+    <div>
+        <h1>{{ title }} Page</h1>
+        <pre>
+        <form>
+            <label for="">FirstName :</label>
+            <input type="text" name="fname" id="fname" v-model="fName">
+
+            <label>LastName :</label>
+            <input type="text" name="lname" id="lname" v-model="lName">
+
+            <button @click.prevent="formAction">{{ title }} record</button>   
+            {{ existingPerson }}         
+        </form>
+    </pre>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "userForm",
+    props: ['title', 'existingPerson'],
+
+    data() {
+        return {
+            fName: "",
+            lName: ""
+        }
+    },
+    watch:{
+        // existingPerson:{
+        //     handler(newValue){
+        //         console.log(newValue)
+        //         this.fName= newValue.fName;
+        //     },
+        //     immediate:true,
+        // }
+    },
+    mounted() {
+        console.log("create");
+
+        console.log(this.existingPerson)
+        // console.log(Object.values(this.existingPerson.fName))
+
+        // let person = JSON.parse(this.existingPerson);
+        this.fName = "vishal";
+        this.lName = "sharma"
+        // this.lName = this.existingPerson.lName;
+    },
+    methods: {
+        formAction() {
+            console.log("button click");
+            if (this.title == "Add") {
+                let person = {
+                    fName: this.fName,
+                    lName: this.lName,
+                }
+                console.log(person)
+                this.$emit("addUser", person);
+            }
+            if(this.title == "Edit"){
+
+                //validation
+                let person = {
+                    fName: this.fName,
+                    lName: this.lName,
+                }
+                console.log(person)
+                this.$emit("editUser", person);
+            }
+
+        },
+    }
+
+}
+</script>
