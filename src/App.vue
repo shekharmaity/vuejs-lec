cb8811f2313d64026cb36168693375e7556ce311
Shekhar Maity <sheku@1921680104.local>
2023-10-29 14:51:57 +0530

    feature-lifecycle hook lec

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,64 @@
 <template>
-  <div></div>
-  
+  <div style="text-align: center;">
+    <h2>Hello World</h2>
+
+    <button @click="isChild = false">hide child</button>
+    <child v-if="isChild"></child>
+
+    <input type="text" name="" id="" v-model="location">
+    <br><br>
+    Location: {{ location }}
+
+  </div>
 </template>
 
 <script>
-
-
+import Child from './components/Child.vue';
 export default {
+  components: { Child },
   name: 'App',
 
-  data(){
-    return{
-      
+  data() {
+    return {
+      isChild: true,
+      location: "",
     }
+  },
+
+  beforeCreate() {
+    console.log("parent before created");
+  },
+
+  created() {
+    console.log("parent created");
+  },
+
+  beforeUpdate() {
+    console.log("parent before update");
+  },
+
+  updated() {
+    console.log("parent update");
+  },
+
+  beforeMount() {
+    console.log("parent before Mount");
+  },
+
+  mounted() {
+    console.log("parent Mounted");
+  },
+
+  beforeUnmount() {
+    console.log("parent before Unmount");
+  },
+
+  unmounted() {
+    console.log("parent unmounted");
   }
-  
+
+
+
 }
 </script>
 
---
+++ src/components/Child.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <h2>Child component</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ChildNode",
+
+    beforeCreate() {
+        console.log("child before created");
+    },
+
+    created() {
+        console.log("child created");
+    },
+
+    beforeUpdate() {
+        console.log("child before update");
+    },
+
+    updated() {
+        console.log("child update");
+    },
+
+    beforeMount() {
+        console.log("child before Mount");
+    },
+
+    mounted() {
+        console.log("child Mounted");
+    },
+
+    beforeUnmount() {
+        console.log("child before Unmount");
+    },
+
+    unmounted() {
+        console.log("child unmounted");
+    }
+}
+
+
+
+</script>
+
+<style lang="scss" scoped></style>
